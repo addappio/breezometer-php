d7551204165bd1e9f721551c03404f7eb065e390
Nicolas Widart <n.widart@gmail.com>
2015-03-26 20:52:47 +0100

    Add some basic test

--- tests/BreezometerTest.php
+++ tests/BreezometerTest.php
@@ -1,6 +1,40 @@
 <?php namespace Nwidart\Breezometer\Tests;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\MessageFactory;
+use GuzzleHttp\Subscriber\Mock;
+use Nwidart\Breezometer\Breezometer;
+
 class BreezometerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Breezometer
+     */
+    private $breezometer;
 
+    public function setUp()
+    {
+        $breezometer = new Breezometer('123');
+
+        $client = new Client();
+
+        $fake = file_get_contents(__DIR__ . '/fixtures/new-york.json');
+        $factory = new MessageFactory();
+        $response = $factory->createResponse(200, ['Content-Type' => 'application/javascript'], $fake);
+
+        $mock = new Mock([$response]);
+        $client->getEmitter()->attach($mock);
+
+        $breezometer->setClient($client);
+
+        $this->breezometer = $breezometer;
+    }
+
+    /** @test */
+    public function it_should_return_decoded_json()
+    {
+        $response = $this->breezometer->baqi('40.7324296', '-73.9977264');
+
+        $this->assertArrayHasKey('country_name', $response);
+    }
 }
