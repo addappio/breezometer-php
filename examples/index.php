cf91f711481ea20ffa12459a2e6b7b521ee2d505
Nicolas Widart <n.widart@gmail.com>
2015-03-26 20:06:56 +0100

    Add an example file

---
+++ examples/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$breezometer = new \Nwidart\Breezometer\Breezometer('your_api_key');
+
+// $info = $breezometer->baqi('40.7324296', '-73.9977264');
+
+$info = $breezometer->baqiFromLocation('New+York');
+
+var_dump($info); exit;
